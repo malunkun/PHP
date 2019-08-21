e879792f07880b7049896f7c8e76357c9228ac57
malunkun <209446860@qq.com>
2019-08-21 21:58:07 +0800

    all

--- web/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "./systemctl.php";
-
-$wifi = new wifi;
-echo $wifi->getWifiName().PHP_EOL;
-echo $wifi->getWifiPasswd().PHP_EOL;
-echo $wifi->getWifiChannel().PHP_EOL;
-echo $wifi->getWifiWpakey().PHP_EOL;
-?>
