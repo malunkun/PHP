e6b7c8809fe847e0a290dd4ab697673675c9bbb6
“malunkun” <209446860@qq.com>
2019-09-08 20:56:58 +0800

    set

--- wan.php
+++ wan.php
@@ -12,5 +12,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $arr = array($type,$ip,$mask,$rount,$dns);
         echo json_encode($arr);
     }
+    if(!empty($_POST["set"])){//4G拨号不支持
+        if($_POST["set"][0] == 1)
+            echo json_encode($wan->setDHCPDNS($_POST["setDHCP"][2]));
+        if($_POST["set"][0] == 2){
+            if($wan->setStaticIP($_POST["set"][1],$_POST["set"][2]) && $wan->setStaticRoute($_POST["set"]))
+                return json_encode("true");
+            else
+                return json_encode("false");
+        }
+    }
 }
 ?>
